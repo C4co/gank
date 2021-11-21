da5a7bac7b1ffb6b9bf35103e8954582cecf30ca
Carlos Costa <carllos.nc@gmail.com>
2021-11-21 16:51:38 -0300

    build: set nuxt ssr to true

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: false,
+  ssr: true,
 
   env: {
     API_KEY: process.env.API_KEY
