a277c0c812cf573fe849eb518dfc6936bc4bd65f
Carlos Costa <carllos.nc@gmail.com>
2021-12-01 01:13:32 -0300

    refactor: remove fetch from server and add fetch from client

--- pages/calendar/_year/_month/index.vue
+++ pages/calendar/_year/_month/index.vue
@@ -2,11 +2,15 @@
 import Vue from 'vue'
 import InfiniteLoading from 'vue-infinite-loading'
 import { calendar } from '@/queries/index'
+const range = require('lodash.range')
 
 type Data = {
   games?: any
-  infiniteId: any,
+  infiniteId: any
+  months: any[]
   nextRequest: string
+  validYears: Array<number>
+  validMonths: Array<number>
 }
 
 export default Vue.extend({
@@ -18,16 +22,40 @@ export default Vue.extend({
     return {
       games: undefined,
       infiniteId: +new Date(),
-      nextRequest: ''
+      nextRequest: '',
+      months: [],
+      validYears: range(1980, 2022),
+      validMonths: range(1, 13)
     }
   },
 
   async fetch(){
-    const { year, month } = this.$route.params
-    const gameRes = await this.$axios.$get(`/games/calendar/${year}/${month}`, calendar(1))
+    try {
+      const { year, month } = this.$route.params
 
-    this.nextRequest = gameRes.next || null
-    this.games = gameRes.results
+      if(!year || !this.validYears.includes(Number(year))){
+        throw new Error('Page don\'t exists')
+      }
+
+      if(!month || !this.validMonths.includes(Number(month))){
+        throw new Error('Page don\'t exists')
+      }
+
+      const gameRes = await this.$axios.$get(`/games/calendar/${year}/${month}`, calendar(1))
+
+      this.nextRequest = gameRes.next || null
+      this.games = gameRes.results
+      this.months = gameRes.months
+
+    } catch (error) {
+      return this.$nuxt.error({ statusCode: 404, message: 'Page not found' })
+    }
+  },
+
+  mounted(){
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+    })
   },
 
   methods: {
@@ -44,25 +72,47 @@ export default Vue.extend({
     }
   },
 
-  fetchOnServer: true,
-  fetchKey: 'calendar-page',
-
-  mounted(){
-    console.log(this.$route.params)
-  }
+  fetchOnServer: false,
+  fetchKey: 'calendar',
 })
 </script>
 
 <template>
   <div>
-    <p v-for="(game, index) in games" :key="`${index}-${game.id}`" >
-      <NuxtLink :to="`games/${game.id}`" >
-        {{ game.name }} - {{ game.released }}
-      </NuxtLink>
-    </p>
+    <h1 v-if="$fetchState.pending"> loading calendar </h1>
+    <h1 v-else-if="$fetchState.error"> Error </h1>
 
-    <ClientOnly>
-      <infinite-loading :identifier="infiniteId" @infinite="infiniteHandler"> </infinite-loading>
-    </ClientOnly>
+    <div v-else>
+      <h1> Calendar </h1>
+
+      <div class="menu">
+        <p v-for="item in months" :key="item.year + item.month">
+          <NuxtLink :to="`/calendar/${item.year}/${item.month}`">
+            {{ item.year }}/{{ item.month }}
+          </NuxtLink>
+        </p>
+      </div>
+
+      <p v-for="(game, index) in games" :key="`${index}-${game.id}`" >
+        <NuxtLink :to="`/games/${game.id}`" >
+          {{ game.name }} - {{ game.released }}
+        </NuxtLink>
+      </p>
+
+      <ClientOnly>
+        <infinite-loading :identifier="infiniteId" @infinite="infiniteHandler"> </infinite-loading>
+      </ClientOnly>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+  .menu{
+    border: solid red 1px;
+    display: flex;
+    gap: 10px;
+    padding: 10px;
+  }
+
+</style>
