89848350fb4cb299ad6a4e921f797f32aa55eb34
Carlos Costa <carllos.nc@gmail.com>
2021-12-01 01:10:28 -0300

    fix(lint): remove console.log

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,3 @@
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  mounted(){
-    console.log('Error page!')
-  }
-})
-</script>
-
 <template>
   <div>
     <h1> Error page! </h1>
