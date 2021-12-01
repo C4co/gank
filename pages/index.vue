92dd4a2095dbbe67e9e28537060aa7dbf01dc460
Carlos Costa <carllos.nc@gmail.com>
2021-12-01 01:31:05 -0300

    refactor: remove platforms

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <script lang="ts">
 import Vue from 'vue'
 import InfiniteLoading from 'vue-infinite-loading'
-import { gamesTrending, genres, platforms } from '@/queries/index'
+import { gamesTrending, genres } from '@/queries/index'
 import { Collection } from '@/types/index'
 
 type Data = {
   games?: any
   genres?: Collection
-  platforms?: Collection
   infiniteId: any,
   nextRequest: string
 }
@@ -21,21 +20,18 @@ export default Vue.extend({
     return {
       games: undefined,
       genres: undefined,
-      platforms: undefined,
       infiniteId: +new Date(),
       nextRequest: ''
     }
   },
 
   async fetch(){
-    const gameRes = await this.$axios.$get('/games/lists/main', gamesTrending(1))
-    const genresRes = await this.$axios.$get('/genres', genres)
-    const platformsRes = await this.$axios.$get('/platforms/lists/parents', platforms)
+    const gameResponse = await this.$axios.$get('/games/lists/main', gamesTrending(1))
+    const genresResponse = await this.$axios.$get('/genres', genres)
 
-    this.nextRequest = gameRes.next || null
-    this.games = gameRes.results
-    this.genres = genresRes.results
-    this.platforms = platformsRes.results
+    this.nextRequest = gameResponse.next || null
+    this.games = gameResponse.results
+    this.genres = genresResponse.results
   },
 
   methods: {
@@ -53,14 +49,26 @@ export default Vue.extend({
   },
 
   fetchOnServer: true,
-  fetchKey: 'home-page'
+  fetchKey: 'home'
 })
 </script>
 
 <template>
   <div>
+    <h1> Trendings </h1>
+
+    <div class="menu">
+      <NuxtLink to="/platforms"> Platforms </NuxtLink>
+      <br>
+      <NuxtLink to="/calendar/2021/10"> Calendário 2021/10 </NuxtLink>
+      <br>
+      <NuxtLink to="top/all-time"> Bests of all time </NuxtLink>
+      <br>
+      <NuxtLink to="/top/2021"> Bests of 2021 </NuxtLink>
+    </div>
+
     <p v-for="(game, index) in games" :key="`${index}-${game.id}`" >
-      <NuxtLink :to="`games/${game.id}`" >
+      <NuxtLink :to="`/games/${game.id}`" >
         {{ game.name }}
       </NuxtLink>
     </p>
@@ -70,3 +78,12 @@ export default Vue.extend({
     </ClientOnly>
   </div>
 </template>
+
+<style lang="scss">
+  .menu {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    border: solid red 1px;
+  }
+</style>
