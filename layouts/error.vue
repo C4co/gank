c46bfea12c930bc966adafcf6c3c923daf621946
Carlos Costa <carllos.nc@gmail.com>
2021-12-01 01:06:02 -0300

    feat(layout): add error page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  mounted(){
+    console.log('Error page!')
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1> Error page! </h1>
+  </div>
+</template>
